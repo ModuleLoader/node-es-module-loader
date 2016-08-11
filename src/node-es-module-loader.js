fc36c00d8539804967a3fe0014380c3b74b3c4ac
guybedford <guybedford@gmail.com>
2016-08-11 16:10:31 +0200

    processRegisterQueue -> processRegisterContext

--- src/node-es-module-loader.js
+++ src/node-es-module-loader.js
@@ -67,7 +67,7 @@ NodeESModuleLoader.prototype.instantiate = function(key, metadata) {
       // evaluate without require, exports and module variables
       // we leave module in for now to allow module.require access
       eval('var require,exports;' + output.code + '\n//# sourceURL=' + key + '!transpiled');
-      loader.processRegisterQueue(key);
+      loader.processRegisterContext(key);
       
       resolve();
     }, reject);
